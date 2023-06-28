b367b86276ee42f4edf714307915d5d0cf1095ee
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-06-28 14:54:37 +0200

    Navbar structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page404 from "./pages/Page404";
-
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,81 @@
+function Navbar() {
+  return (
+    <div className="bg-[#002743] text-white flex flex-col w-64 p-8 gap-8 h-[100vh] fixed">
+      <div className="flex flex-col gap-8">
+        <img
+          className="h-[80px]"
+          src="/assets/images/navbar/logo_emmaus.svg"
+          alt=""
+        />
+        <h1 className="text-center">
+          Bienvenue <br /> Ma Couille
+        </h1>
+        <button className="flex">
+          <img
+            className="px-3"
+            src="/assets/images/navbar/logo_add.svg"
+            alt=""
+          />
+          Nouvelle entrée
+        </button>
+      </div>
+      <div className=" flex flex-col gap-8">
+        <button className="flex">
+          <img
+            className="px-3"
+            src="/assets/images/navbar/logo_faq.svg"
+            alt=""
+          />
+          FAQ
+        </button>
+        <button className="flex">
+          <img
+            className="px-3"
+            src="/assets/images/navbar/logo_parametre.svg"
+            alt=""
+          />
+          Paramètres
+        </button>
+        <button className="flex">
+          <img
+            className="px-3"
+            src="/assets/images/navbar/logo_deconnexion.svg"
+            alt=""
+          />
+          Déconnexion
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+
+{
+  /* <div className="bg-[#002743] h-96 w-64 flex flex-col">
+      <div className="flex flex-col items-center">
+        <img height="80" src="/assets/images/navbar/logo_emmaus.svg" alt="" />
+        <h1>
+          Bienvenue <br /> Ma Couille
+        </h1>
+        <button className="flex gap-8">
+          <img src="/assets/images/navbar/logo_add.svg" alt="" />
+          Nouvelle entrée
+        </button>
+      </div>
+      <div className="flex flex-col gap-5">
+        <button className="flex gap-8">
+          <img src="/assets/images/navbar/logo_faq.svg" alt="" />
+          FAQ
+        </button>
+        <button className="flex gap-8">
+          <img src="/assets/images/navbar/logo_parametre.svg" alt="" />
+          Paramètres
+        </button>
+        <button className="flex gap-8">
+          <img src="/assets/images/navbar/logo_deconnexion.svg" alt="" />
+          Déconnexion
+        </button>
+      </div>
+    </div> */
+}
