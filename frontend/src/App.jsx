e01921739cf9ec0ff6db3d132aa2ed692072ffb1
Samuel Faber <samuel.faber@live.fr>
2023-06-28 16:51:17 +0200

    Ajout de commentaires sur App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 
-import PageConnexion from "./pages/PageConnexion";
+// import des layouts
 import LayoutMain from "./layouts/LayoutMain";
 import LayoutConnect from "./layouts/LayoutConnect";
+// import des pages
+import PageConnexion from "./pages/PageConnexion";
 import UsersMetierPage from "./pages/UsersMetierPage";
-import CaracteristiquesPage from './pages/CaracteristiquesPage';
+import CaracteristiquesPage from "./pages/CaracteristiquesPage";
+// import des composants
 import Model from "./components/Model";
+// import de la 404
 import Page404 from "./pages/Page404";
+
+// import du CSS
 import "./App.css";
 
 function App() {
@@ -14,6 +20,7 @@ function App() {
   const isLogged = location.pathname.startsWith("/app");
 
   return isLogged ? (
+    // Routes principales une fois loggé
     <LayoutMain>
       <Routes>
         <Route path="/app/" element={<UsersMetierPage />} />
@@ -26,6 +33,7 @@ function App() {
       </Routes>
     </LayoutMain>
   ) : (
+    // Routes de connexion et oublie mot de passe
     <LayoutConnect>
       <Routes>
         <Route path="/" element={<PageConnexion />} />
