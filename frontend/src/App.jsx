636aae0054368f9845eb869a683c222fa27b37f3
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-06-28 16:45:06 +0200

    Caracteristique page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Page404 from "./pages/Page404";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Page404 from './pages/Page404';
 
-import "./App.css";
+import './App.css';
+import CaracteristiquesPage from './pages/CaracteristiquesPage';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/caracteristiquespage"
+          element={<CaracteristiquesPage />}
+        />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        raleway: ["Raleway, sans-serif"],
+        raleway: ['Raleway, sans-serif'],
       },
-    },
-    colors: {
-      primary: "#202020",
-      secondary: "#FFFFFF",
-      tertiary: "#e4c467",
     },
   },
   plugins: [],
