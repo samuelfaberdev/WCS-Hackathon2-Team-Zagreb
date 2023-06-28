caa48038ae2fccb31696232f740aac7b9d5545b8
Samuel Faber <samuel.faber@live.fr>
2023-06-28 22:33:08 +0200

    Ajout d'un border bottom sur les collapsible

--- frontend/src/pages/FAQPage.jsx
+++ frontend/src/pages/FAQPage.jsx
@@ -1,4 +1,4 @@
-import Collapsible from 'react-collapsible';
+import Collapsible from "react-collapsible";
 
 export default function FAQPage() {
   return (
@@ -9,10 +9,10 @@ export default function FAQPage() {
       <div className="w-[80%] flex flex-col">
         <Collapsible
           trigger="Qu'est ce [votre produit/service] et comment fonctionne-t-il?"
-          triggerClassName="toto bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="toto bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="toto bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="toto bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -27,10 +27,10 @@ export default function FAQPage() {
         </Collapsible>
         <Collapsible
           trigger="Comment puis-je contacter le support client de [votre produit/service]"
-          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -45,10 +45,10 @@ export default function FAQPage() {
         </Collapsible>
         <Collapsible
           trigger="Comment puis-je supprimer mon compte?"
-          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -63,10 +63,10 @@ export default function FAQPage() {
         </Collapsible>
         <Collapsible
           trigger="Y a t-il des tutoriels ou des ressources d'apprentissage disponibles pour m'aider a utiliser [votre produit/service]"
-          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -81,10 +81,10 @@ export default function FAQPage() {
         </Collapsible>
         <Collapsible
           trigger="Comment puis-je réinitialiser mon mot de passe?"
-          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
@@ -99,10 +99,10 @@ export default function FAQPage() {
         </Collapsible>
         <Collapsible
           trigger="Comment puis-je mettre à jour mon compte ou mes informations personnelles?"
-          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
-          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-5 p-4 rounded-lg"
+          triggerClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
+          triggerOpenedClassName="bg-[#00ACB0] text-white flex w-full font-medium text-2xl mb-4 p-4 rounded-lg"
         >
-          <p className="mb-5">
+          <p className="mb-4 pb-4 border-b-2 border-[#00ACB0]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
             risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
             nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas
