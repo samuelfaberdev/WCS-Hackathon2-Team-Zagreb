6ffca628790e3971d61cea81759455cd264478e4
Samuel Faber <samuel.faber@live.fr>
2023-06-28 11:40:39 +0200

    Exemple Composant

---
+++ frontend/components/ExampleComponents.jsx
@@ -0,0 +1,11 @@
+export default function ExempleComponents() {
+  return (
+    <>
+      <h3 className="font-bold">Test</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos optio quam
+        numquam et beatae deleniti.
+      </p>
+    </>
+  );
+}
