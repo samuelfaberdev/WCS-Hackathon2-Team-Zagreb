2615c53aab1c68d8bed70b2d2ca20b5a3e113494
Victor <fabier.victor.1720@gmail.com>
2023-06-28 16:12:35 +0200

    Page connexion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page404 from "./pages/Page404";
+import PageConnexion from "./pages/PageConnexion"
 
 import "./App.css";
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<Page404 />} />
+        <Route path="/connexion" element={<PageConnexion />} />
       </Routes>
     </>
   );
