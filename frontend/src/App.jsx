77a48aafac690cb83cbe138584b9bad7e8de5b9d
Samuel Faber <samuel.faber@live.fr>
2023-06-28 21:58:52 +0200

    Réajustement mineur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,17 +8,17 @@ import PageConnexion from "./pages/PageConnexion";
 import UsersMetierPage from "./pages/UsersMetierPage";
 import PageMotDePasseOublier from "./pages/PageMotDePasseOublier";
 import CaracteristiquesPage from "./pages/CaracteristiquesPage";
+import PageNouveauMotDePasse from "./pages/PageNouveauMotDePasse";
 import Model from "./pages/Model";
 import Systeme from "./pages/Systeme";
+import FAQPage from "./pages/FAQPage";
 // import des composants
 import SmartPhone from "./components/SmartPhone";
 // import de la 404
 import Page404 from "./pages/Page404";
-import PageNouveauMotDePasse from "./pages/PageNouveauMotDePasse";
 
 // import du CSS
 import "./App.css";
-import FAQPage from "./pages/FAQPage";
 
 function App() {
   const location = useLocation();
@@ -46,8 +46,8 @@ function App() {
     // Routes de connexion et oublie mot de passe
     <LayoutConnect>
       <Routes>
-      <Route path="/nouveau-motdepasse" element={<PageNouveauMotDePasse />} />
-      <Route path="/renitialiser" element={<PageMotDePasseOublier />} />
+        <Route path="/nouveau-motdepasse" element={<PageNouveauMotDePasse />} />
+        <Route path="/renitialiser" element={<PageMotDePasseOublier />} />
         <Route path="/" element={<PageConnexion />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
