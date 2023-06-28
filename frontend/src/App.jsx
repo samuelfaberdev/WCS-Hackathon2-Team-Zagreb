d9fc5b065c10ee1efc93491bd7637983630350c9
Samuel Faber <samuel.faber@live.fr>
2023-06-28 14:53:55 +0200

    Création des Layouts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,33 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+import LayoutMain from "./layouts/LayoutMain";
+import LayoutConnect from "./layouts/LayoutConnect";
+import Connexion from "./pages/Connexion";
+import UsersMetierPage from "./pages/UsersMetierPage";
 import Page404 from "./pages/Page404";
 
 import "./App.css";
-import UsersMetierPage from "./pages/UsersMetierPage";
 
 function App() {
-  return (
+  const location = useLocation();
+  const isLogged = location.pathname.startsWith("/app");
+
+  return isLogged ? (
     <>
+      <LayoutMain>
+        <Routes>
+          <Route path="/app/" element={<UsersMetierPage />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </LayoutMain>
+    </>
+  ) : (
+    <LayoutConnect>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/user-metier" element={<UsersMetierPage />} />
+        <Route path="/" element={<Connexion />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
-    </>
+    </LayoutConnect>
   );
 }
 
