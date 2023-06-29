679a17698f1eddfc57aca276406e52cb44c1b6b6
Alexis Chesneau <[alexischesneau64@gmail.com](mailto:alexischesneau64@gmail.com)>
2023-06-29 10:56:02 +0200

    correction css

--- frontend/src/components/Etat.jsx
+++ frontend/src/components/Etat.jsx
@@ -1,29 +1,31 @@
 export default function Etat() {
   return (
     <>
-      <div className="text-white w-full flex items-center justify-center">
-        <button className="w-32 py-2 bg-[#002743] mr-8 rounded-md">
+      <div className="text-white w-full flex items-center justify-center gap-8">
+        <button className="w-32 py-2 font-bold bg-[#002743] rounded-md">
           Parfait état
         </button>
-        <button className="w-32 py-2 bg-[#002743] mr-8 rounded-md">
+        <button className="w-32 py-2 font-bold bg-[#002743] rounded-md">
           Très bon état
         </button>
-        <button className="w-32 py-2 bg-[#002743] mr-8 rounded-md">
+        <button className="w-32 py-2 font-bold bg-[#002743] rounded-md">
           Bon état
         </button>
-        <button className="w-32 py-2 bg-[#002743] mr-8 rounded-md">
+        <button className="w-32 py-2 font-bold bg-[#002743] rounded-md">
           Satisfaisant
         </button>
       </div>
 
-      <h3>Présence d&apos;un chargeur ?</h3>
-      <div>
-        <input type="radio" name="charger" id="chargeryes" />
-        <label htmlFor="chargeryes">Oui</label>
-      </div>
-      <div>
-        <input type="radio" name="charger" id="chargerno" />
-        <label htmlFor="chargerno">Non</label>
+      <h3 className="text-lg font-bold">Présence d&apos;un chargeur ?</h3>
+      <div className="flex gap-8">
+        <div>
+          <input className="mr-2" type="radio" name="charger" id="chargeryes" />
+          <label htmlFor="chargeryes">Oui</label>
+        </div>
+        <div>
+          <input className="mr-2" type="radio" name="charger" id="chargerno" />
+          <label htmlFor="chargerno">Non</label>
+        </div>
       </div>
     </>
   );
