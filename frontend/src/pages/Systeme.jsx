14a66168cab3d084f62236838003ca53c902daf2
Samuel Faber <samuel.faber@live.fr>
2024-04-27 23:23:23 +0200

    Mise à jour des importations de services dans les fichiers CaracteristiquesPage.jsx et Systeme.jsx

--- frontend/src/pages/Systeme.jsx
+++ frontend/src/pages/Systeme.jsx
@@ -1,4 +1,4 @@
-import * as phoneAPI from "@service/PhoneAPI.service";
+import * as phoneAPI from "@services/PhoneAPI.service";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
