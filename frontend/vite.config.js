93add3f4896588135513d24723384f7b6857c3e9
Samuel Faber <samuel.faber@live.fr>
2024-05-04 01:23:51 +0200

    chore: Update import paths in jsconfig.json and vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,17 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": path.resolve(__dirname, "src/assets"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@contexts": path.resolve(__dirname, "src/contexts"),
-      "@services": path.resolve(__dirname, "src/services"),
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@contexts": path.resolve(__dirname, "./src/contexts"),
+      "@services": path.resolve(__dirname, "./src/services"),
     },
   },
 });
