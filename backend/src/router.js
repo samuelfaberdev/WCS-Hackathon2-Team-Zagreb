fa2ad87deaf746833c86107a6a532b8c44e25e8a
Samuel Faber <samuel.faber@live.fr>
2024-05-04 01:25:47 +0200

    chore: Update API endpoint paths in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -9,13 +9,15 @@ const phoneControllers = require("./controllers/phoneControllers");
 // router.put("/items/:id", itemControllers.edit);
 // router.post("/items", itemControllers.add);
 // router.delete("/items/:id", itemControllers.destroy);
-
-router.get("/phones/", phoneControllers.findAllPhones);
-router.get("/phones/marques/:os", phoneControllers.findAllMarques);
-router.get("/phones/models/:marque", phoneControllers.findAllModels);
-router.get("/phones/couleurs/:model", phoneControllers.findAllCouleurs);
-router.get("/phones/rams/:model", phoneControllers.findAllRams);
-router.get("/phones/stockages/:model", phoneControllers.findAllStockages);
-router.get('/phones/antutu/:model', phoneControllers.findAllAntutu);
+router.get("/api", function (req, res) {
+  res.send("hello world");
+});
+router.get("/api/phones/", phoneControllers.findAllPhones);
+router.get("/api/phones/marques/:os", phoneControllers.findAllMarques);
+router.get("/api/phones/models/:marque", phoneControllers.findAllModels);
+router.get("/api/phones/couleurs/:model", phoneControllers.findAllCouleurs);
+router.get("/api/phones/rams/:model", phoneControllers.findAllRams);
+router.get("/api/phones/stockages/:model", phoneControllers.findAllStockages);
+router.get("/api/phones/antutu/:model", phoneControllers.findAllAntutu);
 
 module.exports = router;
